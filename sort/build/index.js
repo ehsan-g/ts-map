907a81be6acf9b9605650ebb504a36d1d7128a65
Ehsan G <ehsan@say.company>
2022-05-07 23:35:06 -0400

    basic sorting algorithim

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,3 +1,28 @@
 "use strict";
-console.log("HI");
-console.log("HI");
+// class Sorter {
+//   collection: number[];
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var k = 0; k < length - 1; k++) {
+                if (this.collection[k] > this.collection[k + 1]) {
+                    var leftNumber = this.collection[k];
+                    this.collection[k] = this.collection[k + 1];
+                    this.collection[k + 1] = leftNumber;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([323, 3, -22, 45]);
+sorter.sort();
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,27 @@
-console.log("HI");
-console.log("HI");
-55555
+// class Sorter {
+//   collection: number[];
+
+//   constructor(collection: number[]) {
+//     this.collection = collection;
+//   }
+// }
+
+class Sorter {
+  constructor(public collection: number[]) {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let k = 0; k < length - 1; k++) {
+        if (this.collection[k] > this.collection[k + 1]) {
+          const leftNumber = this.collection[k];
+          this.collection[k] = this.collection[k + 1];
+          this.collection[k + 1] = leftNumber;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([323, 3, -22, 45]);
+sorter.sort();
+console.log(sorter.collection);
